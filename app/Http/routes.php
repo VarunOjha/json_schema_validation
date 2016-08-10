f035c29e9627e91c7994f8fc09d708f7c462b821
Varun Ojha <varunojha@Varuns-MacBook-Pro.local>
2016-08-10 15:06:25 +0530

    Added user/user address post routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,14 +1,7 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
 
-
+Route::group(['prefix'=>'v1','as'=>'UserAPIs'], function () {
+Route::post('/register', ['as' => 'RegisterUserToServer', 'uses' =>'UserController@register']);
+Route::post('/user/address', ['as' => 'RegisterAddressToServer', 'uses' =>'UserController@registerAddress']);
+});
